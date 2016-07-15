2a125a1409069be50577ddb2cca5e5b6a0ad7bed
Jose Riha <jose@x500c>
2016-07-15 10:25:55 +0200

    Update addon after website changes

--- plugin.video.jaksetodela.cz/resources/lib/jaksetodela.py
+++ plugin.video.jaksetodela.cz/resources/lib/jaksetodela.py
@@ -20,7 +20,7 @@
 # *
 # */
 
-import re,os,urllib,urllib2,cookielib
+import re,urllib2,cookielib
 import util,resolver
 
 from provider import ContentProvider
@@ -38,7 +38,7 @@ class JaksetodelaContentProvider(ContentProvider):
 		
 
 	def search(self,keyword):
-                return self.film(util.request(self._url('search/?search_id='+keyword+'&search_type=search_videos&submit=Hledej')))
+		return self.catl3(util.parse_html(self._url('search/?search_id='+keyword+'&search_type=search_videos&submit=Hledej')))
 		
 			
 	def substr(self,data,start,end):
@@ -46,65 +46,81 @@ class JaksetodelaContentProvider(ContentProvider):
 		i2 = data.find(end,i1+1)
 		return data[i1:i2]
 			
-	def film(self,page):
-		result=[]
-
-
-		data = self.substr(page,'<span class="pagingnav">1</span>','<span class="pagingnav">1</span>')
-		im = data.find('thumb_position_v')
-		if im > 0:
-			pattern='\t<a href="(?P<url>.+?)".+?title=\'(?P<name>.+?)\'.?>[\s|\S]*?<img.*?src="(?P<img>.+?)" id='
-		else:
-			pattern='<a href="(?P<url>.+?)".?>(?P<name>.+?)</a>'
-		for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
-			item = self.video_item()
-			try:
-				item['img'] = self._url(m.group('img'))
-			except:
-				pass
-			item['url'] = self._url(m.group('url'))
-			item['title'] = m.group('name')
-			#item['plot']= m.group('info')
-			result.append(item)
-			
-		return result
-
 	def cat(self,page):
-		result = []
+		"""
+		top level menu - as of 2016 contains Videa, Kategorie and Rady a Tipy
 		
-		data = util.substr(page,'<a href="/kategorie" >Kategorie</a>','<a href="/vase-rady-tipy"')
-		pattern='a href=\'(?P<url>.+?)\'>(?P<name>.+?)</a>'
-		for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
+		cat		catl2		catl3   
+		Videa - 	Nejnovejsi      [video1, video2..]
+				Nejsledovanejsi [video1, video2..]
+                Kategorie - 	Deti a rodina	[video1, video2..]
+				Dum a zahrada	[video1, video2..]
+				Doprava a cestovani
+		Rady a tipy -	Deti a rodina   [article1, article2..]
+
+		"""
+		result = []
+		navigation = page.select('ul.topnav li[class=navigation_title]')
+		for url,title in [(x.find('a')['href'],x.find('a').text) for x in navigation]:
+			# skip Home
+			if title in 'Home' or title in 'Rady a tipy':
+				continue
 			item = self.dir_item()
-			item['title']=m.group('name')
-			item['url'] = '#catl2#'+self._url(m.group('url'))
+			item['title']=title
+			item['url'] = '#catl2#'+self._url(url)
 			result.append(item)
 		return result
 
 	def catl2(self,page):
+		"""
+		Subcategories (level2) - url prefixed with #catl2#
+		"""
 		result = []
-		
-		data = util.substr(page,'<div id="kategorie_obsahf">','</ul>')
-		pattern='a href=\'(?P<url>.+?)\'>(?P<name>.+?)</a>'
-		for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
+		try:
+			navigation = page.select('ul.topnav a[id]')[0].findParent().select('a')
+		except:
+			# this are most likely Rady a tipy
+			navigation = page.select('div.block2 h3 a')
+		for link in navigation[1:]:
+			if not link.text:
+				continue
 			item = self.dir_item()
-			item['title']=m.group('name')
-			item['url'] = '#film#'+self._url(m.group('url'))
+			item['title']=link.text
+			item['url'] = "#catl3#"+self._url(link['href'])
 			result.append(item)
 		return result
 
 	def catl3(self,page):
+		"""
+		List of movies  - url prefixed with #catl3#
+		"""
 		result = []
-		
-		data = self.substr(page,'<span class="pagingnav">1</span>','<span class="pagingnav">1</span>')
-		pattern='\s<a href="(?P<url>.+?)".+?title=\'(?P<name>.+?)\'.?>[\s|\S]*?<img.*?src="(?P<img>.+?)" id='
-		for m in re.finditer(pattern,data,re.IGNORECASE | re.DOTALL ):
+		try:
+			# kategorie
+			navigation = page.select('div.block-padding-lg')[0].select('div')[1].select('div a')
+		except IndexError:
+			# videa
+			navigation = page.select('div.video_thumb_watch a')
+				
+		for vitem in [x for x in navigation if x.img]:
 			item = self.video_item()
-			item['img'] = m.group('img')
-			item['title'] = m.group('name')
-			#item['url'] = '#film#'+self._url(m.group('url'))
-			item['url'] = self._url(m.group('url'))
+			item['img'] = vitem.img['src']
+			item['title'] = vitem['title']
+			item['url'] = self._url(vitem['href'])
 			result.append(item)
+
+		if page.select('a.prevChar'):
+			item = self.dir_item()
+			item['type'] = 'prev'
+			item['url'] = '#catl3#'+self._url(page.select('a.prevChar')[0]['href'])
+			result.append(item)
+
+		if page.select('a.nextChar'):
+			item = self.dir_item()
+			item['type'] = 'next'
+			item['url'] = '#catl3#'+self._url(page.select('a.nextChar')[0]['href'])
+			result.append(item)
+
 		return result
 		
 	
@@ -115,41 +131,37 @@ class JaksetodelaContentProvider(ContentProvider):
 		item['type'] = 'new'
 		item['url']  = '#last#'+self._url('videos/basic/mr')
 		result.append(item)
-		result.extend(self.cat(util.request(self._url('videos'))))
+		result.extend(self.cat(util.parse_html(self._url('videos'))))
 		
 		return result
 	
 	
 	def list(self,url):
-		if url.find('#film#') == 0:
-                        return self.film(util.request(self._url(url[6:])))
 		if url.find('#cat#') == 0:
-                        return self.cat(util.request(self._url(url[5:])))
+			return self.cat(util.parse_html(self._url(url[5:])))
 		if url.find('#catl2#') == 0:
-                        return self.catl2(util.request(self._url(url[7:])))
+			return self.catl2(util.parse_html(self._url(url[7:])))
+		if url.find('#catl3#') == 0:
+			return self.catl3(util.parse_html(self._url(url[7:])))
 		if url.find('#last#') == 0:
-                        return self.film(util.request(self._url(url[6:])))
+			return self.catl3(util.parse_html(self._url(url[6:])))
 		else:
-                        raise Expception("Invalid url, I do not know how to list it :"+url)
+			raise Exception("Invalid url, I do not know how to list it :"+url)
 
-            
-	
 	def resolve(self,item,captcha_cb=None,select_cb=None):
 		item = item.copy()
 		url = self._url(item['url'])
 		print 'URL: '+url
-		hdata = util.request(url)
+		data = util.request(url)
 		
-		data = util.substr(hdata,'<div id="player"','<div id="other" class="domtab">')
-		pattern = 'videoId: \'(?P<vid>.+?)\','
+		pattern = 'videoId: \'(?P<vid>.+?)[\'?]'
 		m = re.search(pattern, data, re.IGNORECASE | re.DOTALL)
 		if not m == None:
 			url = 'http://www.youtube.com/watch?v='+m.group('vid')
-		print url
 		
 		resolved = resolver.findstreams(url,['(?P<url>http://www.youtube.com/watch\?v='+m.group('vid')+')'])
-                result = []
-                try:
+		result = []
+		try:
 			for i in resolved:
 				item = self.video_item()
 				item['title'] = i['name']
@@ -160,9 +172,7 @@ class JaksetodelaContentProvider(ContentProvider):
 		except:
 			print '===Unknown resolver==='
 			
-                if len(result)==1:
-                        return result[0]
-                elif len(result) > 1 and select_cb:
-                        return select_cb(result)
-	
-
+		if len(result)==1:
+			return result[0]
+		elif len(result) > 1 and select_cb:
+			return select_cb(result)
